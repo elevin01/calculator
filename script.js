eb4a2a9ebd84c18a2d53fad78838b22b197cdc3c
elevin01 <emiltlevin@gmail.com>
2023-06-05 01:08:22 -0400

    adding trig and someother stuff

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 window.onload = function() {
     let display = document.getElementById('display');
+    let history = document.getElementById('history');
+    let cheatSheet = document.getElementById('cheat-sheet');
     let buttons = Array.from(document.getElementsByTagName('button'));
     buttons.map(button => {
         button.addEventListener('click', (e) => {
             switch(e.target.innerText){
                 case 'C':
                     display.value = '';
+                    history.innerText = '';
                     break;
                 case '=':
                     try{
+                        history.innerText = display.value + ' =';
                         display.value = eval(display.value);
                     } catch {
                         display.value = "Error"
@@ -16,6 +20,7 @@ window.onload = function() {
                     break;
                 case 'sqrt':
                     try {
+                        history.innerText = 'sqrt(' + display.value + ') =';
                         display.value = Math.sqrt(eval(display.value));
                     } catch {
                         display.value = "Error";
@@ -26,15 +31,18 @@ window.onload = function() {
                     break;
                 case 'log':
                     try {
+                        history.innerText = 'log(' + display.value + ') =';
                         display.value = Math.log10(eval(display.value));
                     } catch {
                         display.value = "Error";
                     }
                     break;
-                    case 'back':
+                case 'back':
                     display.value = display.value.slice(0, -1);
                     break;
-
+                case 'trig':
+                    cheatSheet.style.display = cheatSheet.style.display == 'none' ? 'block' : 'none';
+                    break;
                 default:
                     display.value += e.target.innerText;
             }
