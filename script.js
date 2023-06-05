65dad27a30af242ca6b59aa8b615f91fe5b49bd8
elevin01 <emiltlevin@gmail.com>
2023-06-05 00:45:55 -0400

    making it work

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    let display = document.getElementById('display');
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    buttons.map(button => {
+        button.addEventListener('click', (e) => {
+            switch(e.target.innerText){
+                case 'C':
+                    display.value = '';
+                    break;
+                case '=':
+                    try{
+                        display.value = eval(display.value);
+                    } catch {
+                        display.value = "Error"
+                    }
+                    break;
+                default:
+                    display.value += e.target.innerText;
+            }
+        });
+    });
+}
